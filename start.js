af376751b6c092d30e1ea3abfcf1b2d384cf90e1
Gael Pasgrimaud <gael@gawel.org>
2017-02-13 18:08:22 +0100

    take care of multi-sheets

--- start.js
+++ start.js
@@ -4,9 +4,14 @@ $(document).ready(function() {
     $.ajax({"url": "./_rooms"}).success(function(data) {
         if (data) {
             $.each(data.sort(), function() {
-                if (! /_formdata$/.exec(this)) {
-                    $('#sheets').append(
-                        '<div><a href="' + this + ' ">' + this + '</a></div>');
+                if (! (/_formdata$/.exec(this) || /\./.exec(this)) ) {
+                    var value = '';
+                    if (data.indexOf(this + '.1') === -1) {
+                        value = '<a href="/' + this + ' ">' + this + '</a>';
+                    } else {
+                        value = '<a href="/=' + this + ' ">' + this + '</a>';
+                    }
+                    $('#sheets').append('<div>' + value + '<div>');
                 }
             });
         }
